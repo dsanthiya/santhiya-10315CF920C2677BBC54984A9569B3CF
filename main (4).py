9e8f540bce3a008268b32a468aab4e2de7930060
dsanthiya <145664492+dsanthiya@users.noreply.github.com>
2023-10-06 13:06:52 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,3 @@
+'''Implement a class called player that represents a cricket player. The player class should have a
+method called play() which prints "The player is playing cricket. Derive two classes, Batsman and
+Bowler, from the Player class. Override the play() method in each derived class to print
